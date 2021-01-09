bcd7c532b0737b13f41bbc45844ca59da848dc08
Nithin Premkumar <70209031+nithinpremkumar1999@users.noreply.github.com>
2021-01-10 01:17:33 +0400

    Added read_write file
    Deals with the writing of user input into txt file and reading of txt file

---
+++ read_write.py
@@ -0,0 +1,9 @@
+"""
+NWHacks 2021
+
+This py file deals with writing user input into txt file and reads the txt file
+and used for output.
+
+This py file uses the data written from user.py for writing
+This py file is used by main.py for getting desired output. 
+"""
