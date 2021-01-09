19810d57ffd307e875c873ccea175f86988fe05f
Nithin Premkumar <70209031+nithinpremkumar1999@users.noreply.github.com>
2021-01-10 03:57:22 +0400

    Made skeleton for write_file() and append_file()
    Made some minor changes to main()
    Made how the functions flow.

--- read_write.py
+++ read_write.py
@@ -14,28 +14,58 @@ This py file is used by main.py for getting desired output.
 
 
 
-#def append_file()
-
-def write_file():
+def append_file(file_name):
     """
     After the user is done entering the products, it is written into a txt file
+    Write elements in data into the already existing file
+
+    Argument:
+    file_name - name of file name entered by user (string value)
+    data - product details that user entered (NEED TO DO)
     """
+    if path.exists(file_name):
+        f = open(file_name, "a+")
+        #Now write into this. from the data. TODO.
+        f.close()
+        global condition
+        condition = False #Made to false after entire data has been written into file
+    else:
+        print("File name doesn't exits.\nEither make new file or append to a existing file")
+
+def write_file(file_name):
+    """
+    After the user is done entering the products, it is written into a txt file
+    Write elements in data into the newly made file
+
+    Argument:
+    file_name - name of file name entered by user (string value)
+    data - product details that user entered (NEED TO DO)
+    """
+    if not path.exists(file_name):
+        f = open(file_name, "w+")
+        #Now write into this. from the data. TODO.
+        f.close()
+        global condition
+        condition = False #Made to false after entire data has been written into file
+    else:
+        print("File name already in use.\nEither append to the existing file or make a new file\n")
 
 def main():
     """
     User gets to choose if they want to make a new file or append.
     User can read the file to get desired output
+
+    Argument:
+    data - product details that user entered (NEED TO DO)
     """
-    while True:
+    while condition:
         choice_input = input("A. Make new file \nB. Append on file\n")
         if choice_input == "A":
             new_file_name = input("Write your desired file name (include .txt)\n")
             write_file(new_file_name)
-            break
         elif choice_input == "B":
             file_name = input("Write the file name (include .txt) or file location\n")
             append_file(file_name)
-            break
         else:
             print("Incorrect option\n")
 
